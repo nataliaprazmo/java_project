52e344cbd0fc7983b5c645f9c7e77456cd46ec81
nataliaprazmo <prazmonatalia@gmail.com>
2023-12-05 19:07:11 +0100

    added spring boot
    loaded generated project with packages spring web, dev tools, data jpa, lombok

---
+++ src/main/java/com/vetappointmentsystem/vas/VasApplication.java
@@ -0,0 +1,13 @@
+package com.vetappointmentsystem.vas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vetappointmentsystem/vas/VasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetappointmentsystem.vas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
